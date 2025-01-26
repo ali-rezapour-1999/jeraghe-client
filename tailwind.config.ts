a01c957616b798cf39d27518410ddb37bbfb1cde
alirg1999 <ali.programer.1999@gmail.com>
2025-01-26 22:29:35 +0330

    feat: set config for project

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,22 @@
 import type { Config } from "tailwindcss";
+import { heroui } from "@heroui/react";
 
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        green: "#004B23",
+        blue: "#1334FD",
+        light: "#EDF2FB",
+        dark: "#171717",
       },
     },
   },
-  plugins: [],
+  plugins: [heroui()],
 } satisfies Config;
