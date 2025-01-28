04e24dbdae526db0a206f99eba8791950b3d6671
alirg1999 <ali.programer.1999@gmail.com>
2025-01-28 22:14:09 +0330

    feat : cart items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,21 @@
 
 import MainNavBar from "@/components/nav/navbar";
 import HomeSectionSearch from "@/components/search/homeSectionSearch";
+import HomeShowJobItems from "@/components/section/homeShowJobItems";
 import ProfileAboutMeSection from "@/components/section/ProfileAboutMeSection";
 import ProfileSection from "@/components/section/profileSection";
 import ProfileViewSection from "@/components/section/profileViewSection";
-import JobDetailDrawer from "@/components/sidebar/jobDetailDrawer";
-import { useDisclosure } from "@heroui/react";
 
 const Home = () => {
-  const { isOpen, onClose, onOpen } = useDisclosure();
   return (
     <main>
       <MainNavBar />
       <div className="w-full h-full flex px-10 gap-5 mt-5 ">
-        <section className="hidden md:flex md:w-7/12 xl:w-9/12">
+        <section className="flex flex-col md:w-7/12 xl:w-9/12 h-[1500vh] overflow-y-auto scrollbar-hide">
           <HomeSectionSearch />
-          <JobDetailDrawer onClose={onClose} isOpen={isOpen} />
+          <HomeShowJobItems />
         </section>
-        <section className="hidden md:flex flex-col md:w-5/12 xl:w-3/12 px-4 h-[90vh] overflow-y-auto scrollbar-hide">
+        <section className="hidden md:flex flex-col md:w-5/12 xl:w-3/12 px-4 h-[90vh] sticky top-20 overflow-y-auto scrollbar-hide">
           <ProfileSection />
           <ProfileViewSection />
           <ProfileAboutMeSection />
---
+++ src/components/section/homeShowJobItems.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+import CartItem from "../cart/cartItem";
+import JobDetailDrawer from "../sidebar/jobDetailDrawer";
+import { useDisclosure } from "@heroui/react";
+import CartItemsContainer from "../container/cartItemsContainer";
+
+const HomeShowJobItems: React.FC = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  return (
+    <CartItemsContainer delay={0.5}>
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
+        <JobDetailDrawer onClose={onClose} isOpen={isOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+        <CartItem onOpen={onOpen} />
+      </div>
+    </CartItemsContainer>
+  );
+};
+
+export default HomeShowJobItems;
